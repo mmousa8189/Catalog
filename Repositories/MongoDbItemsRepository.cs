16241e67cc4bcb84b71db4cd2ee888bf97ca1d9f
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-07-04 23:16:18 +0200

    Persisting entities with MongoDB

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private  const string DATABASE_NAME = "catalog";
+        private  const string COLLECTION_NAME = "items";
+        private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(DATABASE_NAME);
+            itemsCollection = database.GetCollection<Item>(COLLECTION_NAME);
+        }
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
+        }
+
+        public Item GetItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return itemsCollection.Find(new BsonDocument()).ToList();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var filter = filterBuilder.Eq(exisitingIitem => exisitingIitem.Id, item.Id);
+            itemsCollection.ReplaceOne(filter, item);
+        }
+    }
+}
