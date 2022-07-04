63bab77e8c32fb8024bc6788e1193d3bd77e4f84
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-07-05 00:05:34 +0200

    Tasks, Async and Await

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Catalog.Dtos;
 using Catalog.Entities;
 using Catalog.Extensions;
@@ -21,22 +22,23 @@ namespace Catalog.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<ItemDto> GetItems()
+        public async Task<IEnumerable<ItemDto>> GetItems()
         {
-            var items = repository.GetItems().Select(item => item.AsDto());
+            var items = (await repository.GetItemsAsync())
+                        .Select(item => item.AsDto());
             return items;
         }
         [HttpGet("{id}")]
-        public ActionResult<ItemDto> GetItem(Guid id)
+        public async Task<ActionResult<ItemDto>> GetItem(Guid id)
         {
-            var item = repository.GetItem(id);
+            var item = await repository.GetItemAsync(id);
             if (item is null)
                 return NotFound();
             return item.AsDto();
         }
 
         [HttpPost]
-        public ActionResult<ItemDto> CreateItem(CreateItemDto createItemDto)
+        public async Task<ActionResult<ItemDto>> CreateItem(CreateItemDto createItemDto)
         {
             Item item = new()
             {
@@ -45,14 +47,14 @@ namespace Catalog.Controllers
                 Price = createItemDto.Price,
                 CreeatedDate = DateTimeOffset.UtcNow,
             };
-            repository.CreateItem(item);
+            await repository.CreateItemAsync(item);
             return CreatedAtAction(nameof(CreateItem), new { id = item.Id }, item.AsDto());
         }
 
         [HttpPut("{id}")]
-        public ActionResult UpdateItem(Guid id, UpdateItemDto updateItemDto)
+        public async Task<ActionResult> UpdateItem(Guid id, UpdateItemDto updateItemDto)
         {
-            var existingItem = repository.GetItem(id);
+            var existingItem = await repository.GetItemAsync(id);
             if (existingItem is null)
                 return NotFound();
 
@@ -61,19 +63,19 @@ namespace Catalog.Controllers
                 Name = updateItemDto.Name,
                 Price = updateItemDto.Price,
             };
-            repository.UpdateItem(updatedItem);
+            await repository.UpdateItemAsync(updatedItem);
             return NoContent();
         }
 
 
         [HttpDelete("{id}")]
-        public ActionResult DeleteItem(Guid id)
+        public async Task<ActionResult> DeleteItem(Guid id)
         {
-            var existingItem = repository.GetItem(id);
+            var existingItem = await repository.GetItemAsync(id);
             if (existingItem is null)
                 return NotFound();
 
-            repository.DeleteItem(id);
+           await repository.DeleteItemAsync(id);
             return NoContent();
         }
     }
--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Catalog.Repositories
 {
@@ -17,32 +18,32 @@ namespace Catalog.Repositories
             IMongoDatabase database = mongoClient.GetDatabase(DATABASE_NAME);
             itemsCollection = database.GetCollection<Item>(COLLECTION_NAME);
         }
-        public void CreateItem(Item item)
+        public async Task CreateItemAsync(Item item)
         {
-            itemsCollection.InsertOne(item);
+           await itemsCollection.InsertOneAsync(item);
         }
 
-        public void DeleteItem(Guid id)
-        {
-            var filter = filterBuilder.Eq(item => item.Id, id);
-            itemsCollection.DeleteOne(filter);
-        }
-
-        public Item GetItem(Guid id)
+        public async Task DeleteItemAsync(Guid id)
         {
             var filter = filterBuilder.Eq(item => item.Id, id);
-            return itemsCollection.Find(filter).SingleOrDefault();
+           await itemsCollection.DeleteOneAsync(filter);
         }
 
-        public IEnumerable<Item> GetItems()
+        public async Task<Item> GetItemAsync(Guid id)
         {
-            return itemsCollection.Find(new BsonDocument()).ToList();
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return await itemsCollection.Find(filter).SingleOrDefaultAsync();
         }
 
-        public void UpdateItem(Item item)
+        public async Task<IEnumerable<Item>> GetItemsAsync()
+        {
+            return await itemsCollection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task UpdateItemAsync(Item item)
         {
             var filter = filterBuilder.Eq(exisitingIitem => exisitingIitem.Id, item.Id);
-            itemsCollection.ReplaceOne(filter, item);
+            await itemsCollection.ReplaceOneAsync(filter, item);
         }
     }
 }
