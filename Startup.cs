474ea59b4f719a06031271eb6afe6cb42cce8bb0
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-07-06 17:03:56 +0200

    Docker

--- Startup.cs
+++ Startup.cs
@@ -68,8 +68,11 @@ namespace Catalog
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
